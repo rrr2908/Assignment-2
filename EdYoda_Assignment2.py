7cb9ba57f6be0ddde5f156c5dfd1e2d82c640f59
rrr2908 <93927812+rrr2908@users.noreply.github.com>
2021-11-14 14:37:47 +0530

    Add files via upload

---
+++ EdYoda_Assignment2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+# Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuple
+roll_list1 = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+roll_list1.sort(key=lambda a: a[1])
+print(roll_list1)
+
+
+# In[31]:
+
+
+# Write a Python program to print a dictionary whose keys should be the alphabet from a-z and the value should be corresponding ASCII values
+my_dict = {}
+for i in range(97, 97 + 26):
+    my_dict[chr(i)] = i
+
+print(my_dict)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
